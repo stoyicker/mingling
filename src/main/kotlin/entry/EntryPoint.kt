a27646042977ddc0add85d8b84ddb28746eb4085
Jorge Antonio Diaz-Benito Soriano <jorge.antonio@cevt.se>
2018-05-20 20:12:06 +0200

    Add dumb time-printing daemon

--- src/main/kotlin/entry/EntryPoint.kt
+++ src/main/kotlin/entry/EntryPoint.kt
@@ -1,5 +1,7 @@
-package main
+package entry
 
-fun main(args: Array<String>) {
-    System.out.println("Hello project")
+import java.util.Date
+
+fun main(args: Array<String>? = null) {
+    println("I am the daemon and the current time is " + Date())
 }
